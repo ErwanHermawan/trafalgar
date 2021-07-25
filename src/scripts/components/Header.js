524d7bbbdb7ad3a83908b8432029c0a3e6dd6e68
ErwanHermawan <erwanhermawan858@gmail.com>
2021-07-25 15:08:14 +0700

    init

---
+++ src/scripts/components/Header.js
@@ -0,0 +1,54 @@
+/* ------------------------------------------------------------------------------
+@name: Header
+@description: Header
+--------------------------------------------------------------------------------- */
+
+// --- utilities
+import {
+  Scrolllable,
+  BrowserCheck
+} from 'utilities';
+
+// --- Header
+const Header = (() => {
+
+  // --- windowTemp
+  let _windowTemp = $(window).width();
+
+  // --- handleNavMenu
+  const handleNavMenu = () => {
+    $('.js-mobile-menu').on('click', (e) => {
+      if ($('body').hasClass('show-nav')) {
+        Scrolllable.enable();
+        $('body').removeClass('show-nav');
+      } else {
+        Scrolllable.disable();
+        $('body').addClass('show-nav');
+      }
+    });
+  }
+
+  // --- handleCheckClass
+  const handleCheckClass = () => {
+    if ($(window).width() >= 992) {
+      if ($('body').hasClass('show-nav')) {
+        Scrolllable.enable();
+        $('body').removeClass('show-nav');
+      }
+    }
+  }
+
+  // --- init
+  const init = () => {
+    handleNavMenu();
+  }
+
+  // --- return
+  return {
+    init,
+    checkClass: handleCheckClass
+  }
+
+})();
+
+export default Header;
---
+++ src/scripts/components/index.js
@@ -0,0 +1,16 @@
+/* ------------------------------------------------------------------------------
+@name: Components Index
+@description: Indexing all components
+--------------------------------------------------------------------------------- */
+
+import WinScroll from "./WinScroll";
+import WindowResize from "./WindowResize";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export {
+  WinScroll,
+  WindowResize,
+  Header,
+  Footer
+};
